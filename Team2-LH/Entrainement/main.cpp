3124ae86bc846649d4e9860772344dd3592090c9
unknown <louis.franc@insa-rouen.fr>
2016-02-11 12:39:16 -0500

    correction

--- Team2-LH/Entrainement/main.cpp
+++ Team2-LH/Entrainement/main.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 
 
-
 int main()
 {
 #define LIGNE 2
@@ -9,21 +8,23 @@ int main()
 #define TROU 0
 #define CARRE 3
 
-    std::freopen("right_angle.in","r",stdin);
-    std::freopen("right_angle.out","w",stdout);
-    uint32_t N,M;
+    std::freopen("ex.in","r",stdin);
+    std::freopen("ex.out","w",stdout);
+    int32_t N,M;
     std::string line;
     std::cin >> N >> M;
     std::cin.ignore();
-    uint32_t vecteur[N+1][M+1];
-    std::pair<std::pair<uint32_t,uint32_t>,std::pair<uint32_t,uint32_t> > scores[N+1][M+1];
-
-    memset(vecteur,0,sizeof(vecteur));
-
-    for(uint32_t i=0; i<N; i++)
+    const int TAILLEMAX = 1000;
+    std::vector<std::vector<int32_t> > vecteur(N+1,std::vector<int32_t>(TAILLEMAX,0));
+    //  int32_t vecteur[N+1][TAILLEMAX];
+#define PAIR std::pair<std::pair<int32_t,int32_t>,std::pair<int32_t,int32_t> >
+    std::vector<std::vector<PAIR > > scores(N+1,std::vector<PAIR >(TAILLEMAX));
+    //   PAIR scores[N+1][M+1];
+#define  MP(A,B) std::make_pair(A,B)
+    for(int32_t i=0; i<N; i++)
     {
         getline(std::cin,line);
-        for(uint32_t j=0 ; j<line.size() ; j++)
+        for(int32_t j=0 ; j < M ; j++)
         {
             if(line.at(j) == '.')
                 vecteur[i][j] = 0;
@@ -41,24 +42,26 @@ int main()
             }
         }
     }
-    uint32_t score_ligne[N+1];
-    uint32_t score_colonne[M+1];
-    uint32_t l,p,r,q=0;
-#define  MP(A,B) std::make_pair(A,B)
-    for(uint32_t i=0; i<N; i++)
+    for(int32_t i=0; i<N; i++)
     {
-        uint32_t j=0;
-        while(j < M)
+        for(int32_t j=0; j<M; j++)
+        {
+            scores[i][j]= MP(MP(-1,TROU),MP(i,j));
+        }
+    }
+    int32_t l;
+    for(int32_t i=0; i<N; i++)
+    {
+        for(int32_t j=0; j<M; j++)
         {
             if(vecteur[i][j] != 0)
             {
                 l=j;
-                while(j < M && vecteur[i][j] !=0)
+                while(j+1 < M && vecteur[i][j+1] !=0)
                 {
                     j++;
                 }
-                q++;
-                for(uint32_t k=l; k<j; k++)
+                for(int32_t k=l; k<=j; k++)
                 {
                     scores[i][k]=MP(MP(j-l+1,LIGNE),MP(i,l));
                 }
@@ -67,26 +70,23 @@ int main()
             {
                 scores[i][j]=MP(MP(0,TROU),MP(i,j));
             }
-            j++;
         }
     }
-    q=0;
-    for(uint32_t i=0; i<M; i++)
+    for(int32_t i=0; i<M; i++)
     {
-        uint32_t j=0;
-        while(j < N)
+        for(int32_t j=0; j<N; j++)
         {
             if(vecteur[j][i] != 0)
             {
                 l=j;
-                while(j < N && vecteur[j][i] !=0)
+                while(j+1 < N && vecteur[j+1][i] !=0)
                 {
                     j++;
                 }
-                uint32_t res = j-l+1;
-                uint32_t compteur=0;
+                int32_t res = j-l;
+                int32_t compteur=0;
                 bool modifie=false;
-                for(uint32_t k=l; k<=j; k++)
+                for(int32_t k=l; k<=j; k++)
                 {
                     if(res >= scores[k][i].first.first)
                         compteur++;
@@ -100,60 +100,63 @@ int main()
                 if(modifie)
                 {
                     // Update les x
-                    for(uint32_t k=l; k<=j ; k++)
+                    for(int32_t k=l; k<=j ; k++)
                     {
-                        uint32_t x = scores[k][i].second.second;
-                        uint32_t length= scores[k][i].first.first;
-                        uint32_t y = scores[k][i].second.first;
-                        for(uint32_t u=x; u<x+length; u++)
+                        int32_t x = scores[k][i].second.first;
+                        int32_t length= scores[k][i].first.first;
+                        int32_t y = scores[k][i].second.second;
+                        for(int32_t u=y; u<y+length; u++)
                         {
-                            scores[k][u]= MP(MP(length-1,LIGNE),MP(y,x));
+                            if(length-1 >= 0)
+                                scores[x][u]= MP(MP(length-1,LIGNE),MP(x,y));
+                            else
+                                scores[x][u]= MP(MP(0,TROU),MP(x,y));
+
                         }
                     }
 
                     // Update les y
-                    for(uint32_t k=l; k<=j; k++)
+                    for(int32_t k=l; k<=j; k++)
                     {
                         scores[k][i]= MP(MP(j-l+1,COLONNE),MP(l,i));
                     }
                 }
-                //q++;
-                //std::cout << "PAINT_LINE "<< i << " " << l << " "<< i << " "<< j << std::endl;
             }
-            j++;
         }
     }
-    //std::cout << std::endl << q<< std::endl;
 
     std::vector<std::string> sol;
-    bool visited[N+1][M+1];
-    memset(visited,false,sizeof(visited));
-    for(int i=0; i<N; i++)
+    std::vector<std::vector<bool> > visited(N,std::vector<bool>(M,false));
+
+    for(int32_t i=0; i<N; i++)
     {
-        for(int j=0; j<M; j++)
+        for(int32_t j=0; j<M; j++)
         {
-            if(visited[i][j] == false && scores[i][j].first.first > 0)
+            if(visited[i][j] == false )
             {
-                int l = scores[i][j].first.first;
+                int32_t l = scores[i][j].first.first;
+                int32_t x = scores[i][j].second.first;
+                int32_t y = scores[i][j].second.second;
+
                 switch(scores[i][j].first.second)
                 {
                 case(1):
                 {
-                    auto s="PAINT_LINE "+ std::to_string(i) + " " + std::to_string(j) +" "+ std::to_string(i+l) + " " + std::to_string(j) + "\n";
+                    auto s="PAINT_LINE "+ std::to_string(x) + " " + std::to_string(y) +" "+ std::to_string(x+l-1) + " " + std::to_string(y) + "\n";
                     sol.push_back(s);
-                    for(int k=i; k<i+l; k++)
+                    for(int32_t k=x; k<=x+l; k++)
                     {
-                        visited[k][j]=true;
+                        visited[k][y]=true;
                     }
                     break;
                 }
                 case(2) :
                 {
-                    auto s="PAINT_LINE "+ std::to_string(i) + " " + std::to_string(j) +" "+ std::to_string(i) + " " + std::to_string(j+l) + "\n";
+                    auto s="PAINT_LINE "+ std::to_string(x) + " " + std::to_string(y) +" "+ std::to_string(x) + " " + std::to_string(y+l-1) + "\n";
                     sol.push_back(s);
-                    for(int k=j; k<j+l; k++)
+                    for(int32_t k=y; k<=y+l; k++)
                     {
-                        visited[i][k]=true;
+                        visited[x][k]=true;
                     }
                     break;
                 }
@@ -161,11 +164,15 @@ int main()
             }
         }
     }
+    std::cout << sol.size() << std::endl;
     std::for_each(sol.begin(),sol.end(),[](auto & x)
     {
         std::cout << x;
     });
-
+#undef TROU
+#undef COLONNE
+#undef CARRE
+#undef LIGNE
     return 0;
 }
 
